396a45eba263258a76642788fcc1c3ae9f00e631
Fabio <fabioamaral.93jr@gmail.com>
2021-06-06 19:35:14 -0300

    removendo 2 console.logs que havia esquecido de retirar

--- paginaLogin.js
+++ paginaLogin.js
@@ -3,8 +3,6 @@ botaoEntrar.addEventListener('click',()=>{
     let usuario = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
     let usuariosCadastrados = JSON.parse(localStorage.getItem('cadastroUsuarios'));
-    console.log(usuariosCadastrados);
-    console.log(usuariosCadastrados[0].emailUsuario);
     
     for (let i=0;i<usuariosCadastrados.length;i++){
         if (usuario == usuariosCadastrados[i].emailUsuario && senha == usuariosCadastrados[i].senhaUsuario ){
