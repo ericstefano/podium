02bf8844a55ac462e09fb338e270ec81797f7f17
lutkenhaus <gustavomayron9@gmail.com>
2021-06-20 19:08:50 -0300

    primeiros passos no mapa

--- paginaMapa.js
+++ paginaMapa.js
@@ -2,109 +2,101 @@ let map;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -33.91662347903106, lng: 151.22879464019775 },
+        center: { lat: -32, lng: 151.22879464019775 },
         zoom: 18,
     });
 }
+// LOCALIZAÇÃO ATUAL
+x = navigator.geolocation;
+x.getCurrentPosition(success, failure);
 
-// MARCADORES DO MAPA
+function success(position) {
+    var myLat = position.coords.latitude;
+    var myLong = position.coords.longitude;
+
+    var coords = new google.maps.LatLng(myLat, myLong);
+
+    var mapOptions = {
+        zoom: 12,
+        center: coords,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var marker = new google.maps.Marker({ map: map, position: coords });
+}
+
+function failure() { }
+// FIM LOCALIZAÇÃO ATUAL
+
+// MARCADOR CUSTOMIZADO
+// https://www.youtube.com/watch?v=Xptz0GQ2DO4&ab_channel=PradipDebnath
+
+// const iconBase = 'icons/';
+// const marker = new google.maps.Marker({
+//     position: myLatLng,
+//     map: map,
+//     icon: `${iconBase}baseline_local_parking_black_48dp.png`
+// })
 
 const iconBase =
     "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
 const icons = {
-    parking: {
-        icon: iconBase + "parking_lot_maps.png",
-    },
-    library: {
-        icon: iconBase + "library_maps.png",
-    },
-    info: {
-        icon: iconBase + "info-i_maps.png",
+    podcast: {
+        icon: iconBase + "/img/podcast1.jpg",
     },
 };
 const features = [
     {
-        position: new google.maps.LatLng(-33.91721, 151.2263),
-        type: "info",
+        position: new google.maps.LatLng(-33.91722, 151.23064),
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91539, 151.2282),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91747, 151.22912),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.9191, 151.22907),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91725, 151.23011),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91872, 151.23089),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91784, 151.23094),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91682, 151.23149),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.9179, 151.23463),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.91666, 151.23468),
-        type: "info",
+        type: "podcast",
     },
     {
         position: new google.maps.LatLng(-33.916988, 151.23364),
-        type: "info",
-    },
-    {
-        position: new google.maps.LatLng(-33.91662347903106, 151.22879464019775),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.916365282092855, 151.22937399734496),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.91665018901448, 151.2282474695587),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.919543720969806, 151.23112279762267),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.91608037421864, 151.23288232673644),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.91851096391805, 151.2344058214569),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.91818154739766, 151.2346203981781),
-        type: "parking",
-    },
-    {
-        position: new google.maps.LatLng(-33.91727341958453, 151.23348314155578),
-        type: "library",
+        type: "podcast",
     },
 ];
 
 // Create markers.
 for (let i = 0; i < features.length; i++) {
     const marker = new google.maps.Marker({
-        position: features[i].position,
+        position: features[i].position.LatLng = [0],
         icon: icons[features[i].type].icon,
         map: map,
     });
