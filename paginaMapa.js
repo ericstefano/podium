5b2fb6034abf6f5063ce67da9d40f044ce916665
lutkenhaus <gustavomayron9@gmail.com>
2021-06-21 19:04:56 -0300

    marcadores e modal adicionados

--- paginaMapa.js
+++ paginaMapa.js
@@ -1,103 +1,179 @@
-let map;
+var map;
+var InforObj = [];
+var centerCoords = {
+    lat: -19.905328282366675,
+    lng: -43.97587664589055
+};
+
+var icons = {
+    podcast: {
+        icon: 'img/podcast-icon2.png'
+    },
+};
+
+var markersOnMap = [
+    {
+        LatLng: [{
+            lat: -19.85205145019345,
+            lng: -43.97841751460232
+        }],
+        type: 'podcast',
+
+    },
+    {
+        LatLng: [{
+            lat: -19.87131996466008,
+            lng: -43.984518954106576
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.88187884554605,
+            lng: -44.00085022825316
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.9020347548531,
+            lng: -44.005545469570286
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.881686872173166,
+            lng: -43.92490980347161
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.888981696801125,
+            lng: -43.924093239764275
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.896084229515576,
+            lng: -43.957368210837906
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.918541492421443,
+            lng: -43.93981209113035
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.932743585147396,
+            lng: -44.050048191619695
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.942338871157848,
+            lng: -44.03637074952194
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.959800795429427,
+            lng: -43.96247173400872
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.935622232151992,
+            lng: -43.92980918571558
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.97188868792483,
+            lng: -43.938383104642526
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.851928188281764,
+            lng: -43.953081251374435
+        }],
+        type: 'podcast'
+    },
+    {
+        LatLng: [{
+            lat: -19.857304363800623,
+            lng: -43.968595961813676
+        }],
+        type: 'podcast',
+    }
+]
+
+window.onload = function () {
+    initMap();
+};
+
+function addMarkerInfo() {
+    for (var i = 0; i < markersOnMap.length; i++) {
+        const marker = new google.maps.Marker({
+            position: markersOnMap[i].LatLng[0],
+            icon: icons[markersOnMap[i].type].icon,
+            map: map,
+            title: `${i + 1}`,
+            optimized: false
+
+        });
+
+        // Abrir/Criar Modal do podcast
+
+        marker.addListener("click", () => {
+            alert(marker.title);
+        });
+    }
+}
+
+// LOCALIZAÇÃO ATUAL
+
+function currentLocation() {
+    x = navigator.geolocation;
+    x.getCurrentPosition(success, failure);
+    function success(position) {
+        let myLat = position.coords.latitude;
+        let myLong = position.coords.longitude;
+
+        let coords = new google.maps.LatLng(myLat, myLong);
+
+        let mapOptions = {
+            zoom: 14,
+            center: coords,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        let marker = new google.maps.Marker({ map: map, position: coords });
+        addMarkerInfo();
+    }
+
+    function failure() { }
+};
+
+// FIM LOCALIZAÇÃO ATUAL
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -32, lng: 151.22879464019775 },
-        zoom: 18,
-    });
-}
-// LOCALIZAÇÃO ATUAL
-x = navigator.geolocation;
-x.getCurrentPosition(success, failure);
-
-function success(position) {
-    var myLat = position.coords.latitude;
-    var myLong = position.coords.longitude;
-
-    var coords = new google.maps.LatLng(myLat, myLong);
-
-    var mapOptions = {
         zoom: 12,
-        center: coords,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    var marker = new google.maps.Marker({ map: map, position: coords });
-}
-
-function failure() { }
-// FIM LOCALIZAÇÃO ATUAL
-
-// MARCADOR CUSTOMIZADO
-// https://www.youtube.com/watch?v=Xptz0GQ2DO4&ab_channel=PradipDebnath
-
-// const iconBase = 'icons/';
-// const marker = new google.maps.Marker({
-//     position: myLatLng,
-//     map: map,
-//     icon: `${iconBase}baseline_local_parking_black_48dp.png`
-// })
-
-const iconBase =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
-const icons = {
-    podcast: {
-        icon: iconBase + "/img/podcast1.jpg",
-    },
-};
-const features = [
-    {
-        position: new google.maps.LatLng(-33.91722, 151.23064),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91539, 151.2282),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91747, 151.22912),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.9191, 151.22907),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91725, 151.23011),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91872, 151.23089),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91784, 151.23094),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91682, 151.23149),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.9179, 151.23463),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.91666, 151.23468),
-        type: "podcast",
-    },
-    {
-        position: new google.maps.LatLng(-33.916988, 151.23364),
-        type: "podcast",
-    },
-];
-
-// Create markers.
-for (let i = 0; i < features.length; i++) {
-    const marker = new google.maps.Marker({
-        position: features[i].position.LatLng = [0],
-        icon: icons[features[i].type].icon,
-        map: map,
+        center: centerCoords,
     });
+    addMarkerInfo();
 }
