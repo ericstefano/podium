e012f68e420b0ebf784f1f8eaf1ee06e15a6a108
lutkenhaus <gustavomayron9@gmail.com>
2021-07-08 17:12:50 -0300

    menu lateral e página usuário

--- paginaLogin.js
+++ paginaLogin.js
@@ -1,14 +1,18 @@
-const botaoEntrar=document.getElementById('btn_entrar');
-botaoEntrar.addEventListener('click',()=>{
+const botaoEntrar = document.getElementById('btn_entrar');
+botaoEntrar.addEventListener('click', () => {
     let usuario = document.getElementById('email').value;
     let senha = document.getElementById('senha').value;
     let usuariosCadastrados = JSON.parse(localStorage.getItem('cadastroUsuarios'));
-    
-    for (let i=0;i<usuariosCadastrados.length;i++){
-        if (usuario == usuariosCadastrados[i].emailUsuario && senha == usuariosCadastrados[i].senhaUsuario ){
+
+    for (let i = 0; i < usuariosCadastrados.length; i++) {
+        if (usuario == usuariosCadastrados[i].emailUsuario && senha == usuariosCadastrados[i].senhaUsuario) {
+            sessionStorage.setItem("autenticacaoStatus", "Autentificado")
+            sessionStorage.setItem("nomeUsuario", `${usuariosCadastrados[i].nome}` + " " + `${usuariosCadastrados[i].sobrenome}`)
             return window.location.replace('paginaInterna.html')
-        }        
-    }   
+
+
+        }
+    }
 
     alert('Dados Incorretos!');
 
